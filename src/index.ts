1ee60bc9cb70f3d19b895dd801ff338bc967b41a
syler <hileo77@gmail.com>
2020-02-02 10:58:45 +0100

    v1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 export class JestStoreLog {
+  /**string `console.log` function. */
   data = '';
   private oldConsoleLog = console['log'];
   constructor() {
     console['log'] = jest.fn(this.Store);
   }
 
-  Store = (inputs: string) => (this.data += inputs);
-
+  private Store = (inputs: string) => (this.data += inputs);
+  /**Restores the `console.log `function. */
   TestEnd() {
     console.log = this.oldConsoleLog;
   }
