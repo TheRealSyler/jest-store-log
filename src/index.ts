e53b049fe2a0a95e780cacdfb4a979315df2a5e4
syler <hileo77@gmail.com>
2020-11-08 22:57:22 +0100

    add mock console.warn adn error.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,68 @@
+interface Options {
+  /**mock the console.error function */
+  error?: boolean;
+  /**mock the console.warn function */
+  warn?: boolean;
+}
+
 export class JestStoreLog {
   /**@deprecated use the logs property instead. */
   data = '';
-
+  /**stores data from the console.log function. */
   logs: any[] = [];
+  /**stores data from the console.warn function, the warn option has to be enabled. */
+  warnings: any[] = [];
+  /**stores data from the console.error function, the error option has to be enabled. */
+  errors: any[] = [];
+  /**stores data from the console.[log|warn|error] functions. */
+  all: any[] = [];
+
   private oldConsoleLog = console['log'];
-  constructor() {
-    console['log'] = jest.fn(this.Store);
+  private oldConsoleError = console['error'];
+  private oldConsoleWarn = console['warn'];
+  constructor(private option?: Options) {
+    console['log'] = jest.fn(this.storeLog);
+    if (this.option) {
+      if (this.option.error) {
+        console['error'] = jest.fn(this.storeError);
+      }
+      if (this.option.warn) {
+        console['warn'] = jest.fn(this.storeWarn);
+      }
+    }
   }
 
-  private Store = (...inputs: any[]) => {
+  private storeLog = (...inputs: any[]) => {
     if (typeof inputs[0] === 'string') {
       this.data += inputs[0];
     }
-
+    this.all.push(...inputs);
     this.logs.push(...inputs);
   };
-  /**Restores the `console.log `function. */
+  private storeError = (...inputs: any[]) => {
+    this.all.push(...inputs);
+    this.errors.push(...inputs);
+  };
+  private storeWarn = (...inputs: any[]) => {
+    this.all.push(...inputs);
+    this.warnings.push(...inputs);
+  };
+
+  /**@deprecated use the restore method instead. */
   TestEnd() {
+    this.restore();
+  }
+  /**Restores the `console.log `function. */
+  restore() {
     console.log = this.oldConsoleLog;
+    if (this.option) {
+      if (this.option.error) {
+        console.error = this.oldConsoleError;
+      }
+      if (this.option.warn) {
+        console.warn = this.oldConsoleWarn;
+      }
+    }
   }
 }
 export default JestStoreLog;
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -13,4 +13,25 @@ test('test', () => {
   expect(a.data).toEqual('awd:awd2');
   expect(a.logs).toStrictEqual(['awd', 'test', ':awd2', { awd: 'test' }]);
   a.TestEnd();
+
+  const b = new JestStoreLog({ error: true });
+  console.error('sad', 'awd');
+  expect(b.errors).toStrictEqual(['sad', 'awd']);
+  b.restore();
+
+  const c = new JestStoreLog({ warn: true });
+  console.warn('sad', 'awd');
+  expect(c.warnings).toStrictEqual(['sad', 'awd']);
+  c.restore();
+
+  const d = new JestStoreLog({ warn: true, error: true });
+  console.warn('warn');
+  expect(d.warnings).toStrictEqual(['warn']);
+  console.log('log');
+  expect(d.logs).toStrictEqual(['log']);
+  console.error('error');
+  expect(d.errors).toStrictEqual(['error']);
+
+  expect(d.all).toStrictEqual(['warn', 'log', 'error']);
+  d.restore();
 });
